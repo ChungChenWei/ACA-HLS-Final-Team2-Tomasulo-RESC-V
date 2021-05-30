fdd760108de310b3c26d3efd6b1defb74e7efc19
Vincent <b07209016@ntu.edu.tw>
2021-05-31 02:37:43 +0800

    20210531 : update ignore. White list for solution directives and script

---
+++ final-project-hls/solution1/script.tcl
@@ -0,0 +1,30 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project final-project-hls
+set_top cpu
+add_files src/CDB.cpp
+add_files src/CDB.hpp
+add_files src/CPU.cpp
+add_files src/CPU.hpp
+add_files src/common.hpp
+add_files src/functional_unit.cpp
+add_files src/functional_unit.hpp
+add_files src/issuer.cpp
+add_files src/issuer.hpp
+add_files src/register_file.cpp
+add_files src/register_file.hpp
+add_files src/reservation_stations.cpp
+add_files src/reservation_stations.hpp
+add_files src/riscv_instr.hpp
+add_files -tb src/testbench.cpp
+open_solution "solution1" -flow_target vivado
+set_part {xc7z020clg400-1}
+create_clock -period 5 -name default
+#source "./final-project-hls/solution1/directives.tcl"
+csim_design
+csynth_design
+cosim_design
+export_design -format ip_catalog
