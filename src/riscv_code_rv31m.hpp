a99d50a5cf76a39adb3251b037ac745c45d259b5
Vincent <b07209016@ntu.edu.tw>
2021-06-05 21:38:41 +0800

    20210605 : RISC-V code update

---
+++ src/riscv_code_rv31m.hpp
@@ -0,0 +1,13 @@
+#ifndef __RISCV_CODE_RV32M_HPP__
+#define __RISCV_CODE_RV32M_HPP__
+
+#include "common.hpp"
+
+// funct3
+const RISCV_code_t F3_MUL = 0b000; // 0x0
+const RISCV_code_t F3_DIV = 0b100; // 0x4
+// funct7
+const RISCV_code_t F7_MUL = 0b0000001; // 0x01
+const RISCV_code_t F7_DIV = 0b0000001; // 0x01
+
+#endif
