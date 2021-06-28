08946f12f57e3615a00d3f0fb399de9e7a42b000
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2021-06-29 02:49:34 +0800

    remove CDB.cpp CDB.hpp; handle register x0

--- src/kernel/CDB.cpp
+++
@@ -1,6 +0,0 @@
-// #include "CDB.hpp"
-
-// void CDB_broadcast (Register_file &rf, Reservation_stations &rs, res_sta_symbol_t sym, data_t value) {
-//     rf.write_from_CDB(sym, value);
-//     rs.write_from_CDB(sym, value);
-// }
