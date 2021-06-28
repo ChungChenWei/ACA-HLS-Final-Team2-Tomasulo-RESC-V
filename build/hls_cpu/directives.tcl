f6065c7203a1af1b1b3d67868c29b7b7ca83eda0
Vincent <b07209016@ntu.edu.tw>
2021-06-29 00:14:35 +0800

    add build dir including hls and vivado

---
+++ build/hls_cpu/directives.tcl
@@ -0,0 +1,8 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_top -name cpu "cpu"
+set_directive_interface -mode s_axilite "cpu" instruction_memory_i
+set_directive_interface -mode s_axilite "cpu" final_register_file_o
---
+++ build/hls_cpu/script.tcl
@@ -0,0 +1,31 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project tomasulo-riscv-cpu
+set_top cpu
+add_files ../../src/kernel/riscv_instr.hpp
+add_files ../../src/kernel/riscv_instr.cpp
+add_files ../../src/kernel/reservation_stations.hpp
+add_files ../../src/kernel/reservation_stations.cpp
+add_files ../../src/kernel/register_file.hpp
+add_files ../../src/kernel/register_file.cpp
+add_files ../../src/kernel/issuer.hpp
+add_files ../../src/kernel/issuer.cpp
+add_files ../../src/kernel/functional_unit.hpp
+add_files ../../src/kernel/functional_unit.cpp
+add_files ../../src/kernel/common.hpp
+add_files ../../src/kernel/CPU.hpp
+add_files ../../src/kernel/CPU.cpp
+add_files ../../src/kernel/CDB.hpp
+add_files -tb ../../src/kernel/testbench.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+open_solution "solution1" -flow_target vivado
+set_part {xc7z020-clg400-1}
+create_clock -period 15 -name default
+config_export -format ip_catalog -output ../cpu.zip -rtl verilog
+source "./directives.tcl"
+csim_design
+csynth_design
+cosim_design -trace_level all
+export_design -rtl verilog -format ip_catalog -output ../cpu.zip
