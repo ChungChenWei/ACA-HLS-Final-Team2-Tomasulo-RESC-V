6852d17ecdd151679a795791b11e912f48a68eda
Vincent <b07209016@ntu.edu.tw>
2021-06-28 16:24:32 +0800

    target time change to 15 ns

--- final-project-hls/solution1/script.tcl
+++ final-project-hls/solution1/script.tcl
@@ -1,31 +1,32 @@
 ############################################################
 ## This file is generated automatically by Vitis HLS.
 ## Please DO NOT edit it.
-## Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+## Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ############################################################
 open_project final-project-hls
 set_top cpu
-add_files src/riscv_instr.hpp
-add_files src/riscv_instr.cpp
-add_files src/reservation_stations.hpp
-add_files src/reservation_stations.cpp
-add_files src/register_file.hpp
-add_files src/register_file.cpp
-add_files src/issuer.hpp
-add_files src/issuer.cpp
-add_files src/functional_unit.hpp
-add_files src/functional_unit.cpp
-add_files src/common.hpp
-add_files src/CPU.hpp
-add_files src/CPU.cpp
-add_files src/CDB.hpp
-add_files src/CDB.cpp
-add_files -tb src/testbench.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files src/kernel/riscv_instr.hpp
+add_files src/kernel/riscv_instr.cpp
+add_files src/kernel/reservation_stations.hpp
+add_files src/kernel/reservation_stations.cpp
+add_files src/kernel/register_file.hpp
+add_files src/kernel/register_file.cpp
+add_files src/kernel/issuer.hpp
+add_files src/kernel/issuer.cpp
+add_files src/kernel/functional_unit.hpp
+add_files src/kernel/functional_unit.cpp
+add_files src/kernel/common.hpp
+add_files src/kernel/CPU.hpp
+add_files src/kernel/CPU.cpp
+add_files src/kernel/CDB.hpp
+add_files CDB.cpp
+add_files -tb src/kernel/testbench.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 open_solution "solution1" -flow_target vivado
 set_part {xc7z020-clg400-1}
-create_clock -period 5 -name default
+create_clock -period 15 -name default
+config_export -format ip_catalog -output D:/HLS/HLSFinal_2021Spring/ACA-HLS-Final-Team2-Tomasulo-RISC-V/CPU-IP/cpu.zip -rtl verilog
 source "./final-project-hls/solution1/directives.tcl"
 csim_design
 csynth_design
-cosim_design -trace_level port
-export_design -format ip_catalog
+cosim_design -trace_level all
+export_design -rtl verilog -format ip_catalog -output D:/HLS/HLSFinal_2021Spring/ACA-HLS-Final-Team2-Tomasulo-RISC-V/CPU-IP/cpu.zip
